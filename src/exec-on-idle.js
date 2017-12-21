50b7ec0d47dd085d6b4dceb3a02811370a924adf
Melvin Sembrano <melvinsembrano@gmail.com>
2017-12-22 10:51:15 +1100

    initial code

--- src/exec-on-idle.js
+++ src/exec-on-idle.js
@@ -2,6 +2,12 @@
 
 function Runner() {
   this.pid = {}
+
+  function exec(method, timeout, name) {
+    let pidName = name || Math.floor(Math.random()*16777215).toString(16)
+    clearTimeout(this.pid[pidName])
+    this.pid[pidName] = setTimeout(method, timeout)
+  }
 }
 
 export default Runner
