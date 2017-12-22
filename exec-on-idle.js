11734abf2a7c82d74322ed83d6cfcccbc6569af6
Melvin Sembrano <melvinsembrano@gmail.com>
2017-12-22 11:44:50 +1100

    added build for production

--- exec-on-idle.js
+++ exec-on-idle.js
@@ -3,14 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-function Runner() {
+function ExecOnIdle() {
   this.pid = {};
 }
 
-Runner.prototype.exec = function (method, timeout, name) {
+ExecOnIdle.prototype.run = function (method, timeout, name) {
   var pidName = name || Math.floor(Math.random() * 16777215).toString(16);
   clearTimeout(this.pid[pidName]);
   this.pid[pidName] = setTimeout(method, timeout);
 };
 
-exports.default = Runner;
+exports.default = ExecOnIdle;
